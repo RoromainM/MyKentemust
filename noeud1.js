e33594b913a7ac956dcb845063c0848abb6e1820
Xenunu <romain.monmarche@efrei.net>
2023-10-26 18:13:15 +0200

    Mise a jour

--- noeud1.js
+++ noeud1.js
@@ -2,12 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
   // Sélectionne les éléments du DOM
   const selectCouleur1 = document.getElementById("selectCouleur1");
   const selectCouleur2 = document.getElementById("selectCouleur2");
-  const resultatNoeudPapillon = document.getElementById("resultatNoeudPapillon");
+  const resultatNoeudPapillon = document.getElementById(
+    "resultatNoeudPapillon"
+  );
   const noeudPapillon = document.getElementById("noeudPapillon");
 
   // Charge l'image par défaut
   const cheminImageDefaut = `Assets/noeud1/bleubleu.jpg`;
   noeudPapillon.src = cheminImageDefaut;
+
+  // Liste des couleurs possibles
+  const couleursPossibles = ["bleu", "bleuciel", "jaune", "terracotta", "rose"];
+
+  // Préchargez les images au chargement de la page
+  prechargerImages(couleursPossibles);
 
   // Écoute les changements dans les sélecteurs de couleur
   selectCouleur1.addEventListener("change", personnaliserNoeudPapillon);
@@ -17,7 +25,6 @@ document.addEventListener("DOMContentLoaded", function () {
     const couleur1 = selectCouleur1.value;
     const couleur2 = selectCouleur2.value;
 
-    
     const cheminImage = `Assets/noeud1/${couleur1}${couleur2}.jpg`;
 
     resultatNoeudPapillon.style.display = "block";
@@ -35,11 +42,20 @@ document.addEventListener("DOMContentLoaded", function () {
     img.src = cheminImage;
   }
 
-  noeudPapillon.style.width = "200px"; 
-  noeudPapillon.style.height = "auto"; 
+  noeudPapillon.style.width = "200px";
+  noeudPapillon.style.height = "auto";
   noeudPapillon.style.margin = "10px";
 
   // Appelez la fonction de personnalisation initialement pour afficher l'image par défaut
   personnaliserNoeudPapillon();
-
+  
+  function prechargerImages(couleurs) {
+    couleurs.forEach((couleur1) => {
+      couleurs.forEach((couleur2) => {
+        const cheminImage = `Assets/noeud1/${couleur1}${couleur2}.jpg`;
+        const img = new Image();
+        img.src = cheminImage;
+      });
+    });
+  }
 });
